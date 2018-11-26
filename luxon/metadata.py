511dd2390595a10a6d39f74e58e1c7c180c13aa8
Christiaan Frans Rademan <christiaan.rademan@gmail.com>
2018-11-26 15:53:14 +0200

    Added Docker functionality / utils

--- luxon/metadata.py
+++ luxon/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'luxon'
 
 # Please follow https://www.python.org/dev/peps/pep-0440/
-version = '1.1.1'
+version = '1.2.0'
 
 author = "Myria Solutions (PTY) Ltd"
 email = 'project@tachyonic.org'
